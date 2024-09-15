cb7e43e733486d9a268178ac9fd135e2ff311fe3
unknown <mrabetyoussef2508@gmail.com>
2024-09-15 18:22:09 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+var typed = new Typed(".typewriter-text ",
+    {
+        strings: [" ",
+            "rapper",
+           
+            "Hip Hop",
+           
+            "",
+           
+            "",
+           
+            ""
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop:true,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+        
+    }
+)
+
+let btnMenu = document.getElementById('btn-menu');
+let Menu = document.querySelector('.nav-links');
+
+btnMenu.onclick = function(){
+    btnMenu.classList.toggle('fa-times');
+    Menu.classList.toggle('active')
+}
+
+let header = document.querySelector('header');
+
+window.onscroll = function(){
+    if(this.scrollY >= 100){
+        header.classList.add('active')
+    }
+    else{
+        header.classList.remove('active')
+    }
+    btnMenu.classList.remove('fa-times')
+    Menu.classList.remove('active')
+}
